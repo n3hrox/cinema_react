fca9a7553d86e3e66a9d24083bde9fe95e170273
Jakub Janik <n3hrox@gmail.com>
2017-11-26 19:48:14 +0100

    Working separate version

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,23 @@ import React from 'react'
 
 const Home = () => (
   <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
-         <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+    <h2>HELLO</h2>
+Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a
+pellentesque dui, non felis. Maecenas malesuada elit lectus felis,
+malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna.
+Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in,
+dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris
+nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam
+erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum
+ id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non,
+ consectetuer lobortis quis, varius in, purus. Integer ultrices posuere cubilia Curae,
+  Nulla ipsum dolor lacus, suscipit adipiscing. Cum sociis natoque penatibus et ultrices
+   volutpat. Nullam wisi ultricies a, gravida vitae, dapibus risus ante sodales lectus
+    blandit eu, tempor diam pede cursus vitae, ultricies eu, faucibus quis, porttitor
+     eros cursus lectus, pellentesque eget, bibendum a, gravida ullamcorper quam. Nullam
+     viverra consectetuer. Quisque cursus et, porttitor risus. Aliquam sem. In hendrerit
+     nulla quam nunc, accumsan congue. Lorem ipsum primis in nibh vel risus. Sed vel lectus.
+     Ut sagittis, ipsum dolor quam.
   </div>
 )
 
--- src/components/Seats.js
+++ src/components/Seats.js
@@ -1,21 +1,75 @@
-import React from 'react';
-import Seatmap from 'react-seatmap';
-import './seats.css'
+import React from 'react'
+import '../styles/seats.css'
 
-var rows = [
-  [{ number: 1 }, {number: 2}, {number: '3', isReserved: true}, null, {number: '4'}, {number: 5}, {number: 6}],
-  [{ number: 1, isReserved: true }, {number: 2, isReserved: true}, {number: '3', isReserved: true}, null, {number: '4'}, {number: 5}, {number: 6}],
-  [{ number: 1 }, {number: 2}, {number: 3, isReserved: true}, null, {number: '4'}, {number: 5}, {number: 6}],
-  [{ number: 1 }, {number: 2}, {number: 3}, null, {number: '4'}, {number: 5}, {number: 6}],
-  [{ number: 1, isReserved: true }, {number: 2}, {number: '3', isReserved: true}, null, {number: '4'}, {number: 5}, {number: 6, isReserved: true}]
-];
+class Seats extends React.Component {
+  constructor() {
+    super();
+      this.state = {
+      seat: [
+        'Front1','Front2','Front3',
+        'Middle1','Middle2','Middle3',
+        'Back1','Back2','Back3'
+      ],
+      seatAvailable: [
+        'Front1','Front2','Front3',
+        'Middle1','Middle2','Middle3',
+        'Back1','Back2','Back3'
+      ],
+      seatReserved: []
+    }
+  }
 
+  onClickData(seat) {
+    if(this.state.seatReserved.indexOf(seat) > -1 ) {
+      this.setState({
+        seatAvailable: this.state.seatAvailable.concat(seat),
+        seatReserved: this.state.seatReserved.filter(res => res != seat)
+      })
+    } else {
+      this.setState({
+        seatReserved: this.state.seatReserved.concat(seat),
+        seatAvailable: this.state.seatAvailable.filter(res => res != seat)
+      })
+    }
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>Seat Reservation System</h1>
+        <DrawGrid
+          seat = { this.state.seat }
+          available = { this.state.seatAvailable }
+          reserved = { this.state.seatReserved }
+          onClickData = { this.onClickData.bind(this) }
+          />
+      </div>
+    )
+  }
+}
 
-const Seats = () => (
-  <div className='seatmap'>
-  <Seatmap rows={rows} maxReservableSeats={3} alpha />
-  </div>
-)
+class DrawGrid extends React.Component {
+  render() {
+    return (
+       <div className="container">
+        <h2></h2>
+        <table className="grid">
+          <tbody>
+              <tr>
+                { this.props.seat.map( row =>
+                  <td
+                    className={this.props.reserved.indexOf(row) > -1? 'reserved': 'available'}
+                    key={row} onClick = {e => this.onClickSeat(row)}>{row} </td>) }
+              </tr>
+          </tbody>
+        </table>
+       </div>
+    )
+  }
+
+  onClickSeat(seat) {
+    this.props.onClickData(seat);
+  }
+}
 
 export default Seats
