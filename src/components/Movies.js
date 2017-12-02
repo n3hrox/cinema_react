8f078b93fc4a914f056b59582d8389afc819e0f4
Jakub Janik <n3hrox@gmail.com>
2017-12-02 13:01:25 +0100

    Working API calls

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,68 +1,26 @@
 import React from 'react'
 import {withRouter} from 'react-router-dom';
 
+var url = 'http://localhost:5000/movies';
 
 class Movies extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: [{
-        id: 1,
-        name: "Simon Bailey"
-      }, {
-        id: 2,
-        name: "Thomas Burleson"
-      }, {
-        id: 3,
-        name: "Will Button"
-      }, {
-        id: 4,
-        name: "Ben Clinkinbeard"
-      }, {
-        id: 5,
-        name: "Kent Dodds"
-      }, {
-        id: 6,
-        name: "Trevor Ewen"
-      }, {
-        id: 7,
-        name: "Aaron Frost"
-      }, {
-        id: 8,
-        name: "Joel Hooks"
-      }, {
-        id: 9,
-        name: "Jafar Husain"
-      }, {
-        id: 10,
-        name: "Tim Kindberg"
-      }, {
-        id: 11,
-        name: "John Lindquist"
-      }, {
-        id: 12,
-        name: "Joe Maddalone"
-      }, {
-        id: 13,
-        name: "Tyler McGinnis"
-      }, {
-        id: 14,
-        name: "Scott Moss"
-      }, {
-        id: 15,
-        name: "Robert Penner"
-      }, {
-        id: 16,
-        name: "Keith Peters"
-      }, {
-        id: 17,
-        name: "Lukas Ruebbelke"
-      }, {
-        id: 18,
-        name: "Brett Shollenberger"
-      }]
+      data: []
     }
+    console.log(this.state.data)
   }
+  componentDidMount() {
+  this.getMovies().then(result => this.setState({
+    data: result
+  }))
+  }
+  getMovies() {
+    return fetch(url).then((resp) => resp.json())
+  }
+
+
   nextPath(path) {
     this.props.history.push(path);
   }
